e72e90879d6bd39485544a146d6f0e87f25e6eab
ssstarss <s.s.star@mail.ru>
2023-12-18 13:05:08 +0700

    solve chain maker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,38 +5,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
-  chain : [],
+  chain :[] ,
+
   getLength() {
-    return this.chain.length
+    return this.chain.split('~~').length
   },
+
   addLink(value) {
-    
-    value.toString();
-    if (typeof value == 'undefined') value  = ' ';
+    if (value == null) value = 'null';
     this.chain.push(value);
     return this;
   },
+
   removeLink(position) {
-    if (position > this.chain.length || position < 0 || typeof position != 'number' || position != Math.floor(position) ) {
-      console.log("You can't remove incorrect link!");
-      return false;
+    if (position > this.chain.length - 1 || position < 1 || typeof position != 'number' || position != Math.floor(position) ) {
+      this.chain = [];
+      throw new Error("You can't remove incorrect link!");
     }
-    delete this.chain[position-1];
+    this.chain.splice(position - 1,1);;
     return this; 
   },
+
   reverseChain() {
-   this.chain.reverse() 
-   return this; 
+    this.chain.reverse() 
+    return this; 
   },
+  
   finishChain() {
-    let result = '';
-    this.chain.forEach(item => result += ('(' + item + ')' +'~~' ));
-    result = result.substring(0,result.length-2);
+    result = '( ' + this.chain.join(' )~~( ') + ' )';
+    this.chain = [];
     return result;
   }
-};
-chainMaker.addLink(function () { }).addLink('2nd').addLink('3rd').removeLink(2).reverseChain().finishChain();
-console.log(chainMaker);
+}
+
 module.exports = {
   chainMaker
 };
